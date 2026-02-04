576dda3f41d90e69c01236a8ad4076629be45fa2
pirojohek <pirojochek01@gmail.com>
2026-02-04 15:50:02 +0700

    renamed class

--- src/main/java/by/pirog/suppliers/spi/spring/jpa/JpaClientFindSupplyById.java
+++ src/main/java/by/pirog/suppliers/spi/spring/jpa/JpaClientFindSupplyById.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
-public class JpaClientFindSupplyByIdSpi implements FindSupplyByIdSpi {
+public class JpaClientFindSupplyById implements FindSupplyByIdSpi {
 
     private final SupplyRepository supplyRepository;
 
